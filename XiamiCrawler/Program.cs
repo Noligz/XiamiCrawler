b2c0b91b19188296f3b78e34fd51cb7d3f9a9b8b
Noligz <noligz@live.cn>
2013-12-08 17:12:16 +0800

    comment

--- XiamiCrawler/Program.cs
+++ XiamiCrawler/Program.cs
@@ -13,27 +13,9 @@ namespace XiamiCrawler
         static void Main(string[] args)
         {
             Crawler crawler = new Crawler();
-            //crawler.SearchArtist();
-            //crawler.GetAlbumFromArtist();
+            crawler.SearchArtist();
+            crawler.GetAlbumFromArtist();
             crawler.GetSongFromAlbum();
-
-            //crawler.GetAlbumLink();
-            //crawler.GetSongInfo();
-
-            //string a = "あくえり２～ AQUAELIE fanDISC vol.2";
-            //string b = "あくえり2 ～ AQUAELIE fanDISC vol.2";
-            //bool bb = crawler.CompareName(a, b);
-            ////a = crawler.TrimKeyword(a);
-
-            //StreamReader sr = new StreamReader("symbol.txt", Encoding.Default);
-            //StreamWriter sw = new StreamWriter("st.txt", false, Encoding.Default);
-            //while (!sr.EndOfStream)
-            //{
-            //    string line = sr.ReadLine();
-            //    //Console.WriteLine(line);
-            //    sw.WriteLine(crawler.ReplaceSymbol(line));
-            //    sw.Flush();
-            //}
         }
 
     }
